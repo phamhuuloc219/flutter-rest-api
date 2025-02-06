417bc75393b1f362094c3aa7c9ebc26d9b2ed8aa
phamhuuloc219 <phamhuuloc2192003@gmail.com>
2025-02-06 23:19:44 +0700

    feat: refresh user

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -27,59 +27,62 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("Flutter REST API"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: ListView.builder(
-        itemCount: users.length,
-        itemBuilder: (context, index) {
-          final user = users[index];
-          // final color = user.gender == 'male' ? Colors.blue : Colors.green;
-          return Card(
-            child: ListTile(
-              leading: Image(image: NetworkImage(user.avt.thumbnail)),
-              title: Text(user.name.first + " " + user.name.last),
-              subtitle: Text(user.phone),
-              trailing: // call button
-              Container(
-                // profile pic of driver
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle
-                ),
-                child: IconButton(
-                    onPressed:() => showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: Image(image: NetworkImage(user.avt.thumbnail),width: 150, height: 150,),
-                        content: SingleChildScrollView(
-                          child: ListBody(
-                            children: [
-                              Text("${user.fullName}", style: TextStyle(fontSize: 20),),
-                              Text('Age: ${user.dob.age}'),
-                              Text("Gender: ${user.gender}"),
-                              Text('Phone: ${user.phone}'),
-                              Text('Email: ${user.email}'),
-                              Text("Address: ${user.location.city}, ${user.location.country}")
-                            ],
+      body: RefreshIndicator(
+        onRefresh: fetchUsers,
+        child: ListView.builder(
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final user = users[index];
+            // final color = user.gender == 'male' ? Colors.blue : Colors.green;
+            return Card(
+              child: ListTile(
+                leading: Image(image: NetworkImage(user.avt.thumbnail)),
+                title: Text(user.name.first + " " + user.name.last),
+                subtitle: Text(user.phone),
+                trailing: // call button
+                Container(
+                  // profile pic of driver
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle
+                  ),
+                  child: IconButton(
+                      onPressed:() => showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Image(image: NetworkImage(user.avt.thumbnail),width: 150, height: 150,),
+                          content: SingleChildScrollView(
+                            child: ListBody(
+                              children: [
+                                Text("${user.fullName}", style: TextStyle(fontSize: 20),),
+                                Text('Age: ${user.dob.age}'),
+                                Text("Gender: ${user.gender}"),
+                                Text('Phone: ${user.phone}'),
+                                Text('Email: ${user.email}'),
+                                Text("Address: ${user.location.city}, ${user.location.country}")
+                              ],
+                            ),
                           ),
+                          actions: [
+                            //cancel
+                            TextButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text('Close',style:TextStyle(color: Colors.red),),
+                            ),
+                          ],
                         ),
-                        actions: [
-                          //cancel
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: Text('Close',style:TextStyle(color: Colors.red),),
-                          ),
-                        ],
                       ),
-                    ),
-                    icon: Icon(Icons.more_vert)
+                      icon: Icon(Icons.more_vert)
+                  ),
                 ),
-              ),
-          ),);
-      },),
+            ),);
+        },),
+      ),
     );
   }
-  void fetchUsers() async{
+  Future<void> fetchUsers() async{
     final response = await UserApi.fetchUsers();
     setState(() {
       users = response;
